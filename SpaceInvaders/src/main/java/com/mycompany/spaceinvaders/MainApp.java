afb2d0429221a1f3498d9a5e49c551e58c989b24
Pate1337 <paavo.hemmo@hotmail.com>
2018-03-28 16:11:22 +0300

    Harjoittelua

--- SpaceInvaders/src/main/java/com/mycompany/spaceinvaders/MainApp.java
+++ SpaceInvaders/src/main/java/com/mycompany/spaceinvaders/MainApp.java
@@ -1,5 +1,6 @@
 package com.mycompany.spaceinvaders;
 
+import java.io.FileInputStream;
 import javafx.scene.paint.Color;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
@@ -11,9 +12,14 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-//import javafx.scene.image.Image;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
+import java.util.ArrayList;
+import javafx.event.EventHandler;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.VBox;
 
 
 public class MainApp extends Application {
@@ -26,10 +32,22 @@ public class MainApp extends Application {
         Scene theScene = new Scene( root );
         stage.setScene( theScene );
         
+//        Canvas canvas = new Canvas(800, 800);
+//        root.getChildren().add( canvas );
+        
+        Image image = new Image(new FileInputStream("/home/paavo/otm-harjoitustyo/SpaceInvaders/space1.jpg"));
+        ImageView imageView = new ImageView(image);
+        imageView.setX(0); 
+        imageView.setY(0);
+        imageView.setFitHeight(800); 
+        imageView.setFitWidth(800);
+        imageView.setPreserveRatio(false);
+        root.getChildren().add(imageView);
+        
         Canvas canvas = new Canvas(800, 800);
         root.getChildren().add( canvas );
         
-        GraphicsContext gc = canvas.getGraphicsContext2D();
+        final GraphicsContext gc = canvas.getGraphicsContext2D();
         gc.setFill(Color.RED);
         gc.setStroke(Color.BLACK);
         gc.setLineWidth(2);
@@ -37,8 +55,49 @@ public class MainApp extends Application {
         gc.setFont( theFont );
         gc.fillText("Press PLAY to start playing!", 30, 370);
         gc.strokeText("Press PLAY to start playing!", 30, 370);
-//        Image space = new Image("space.png");
-//        gc.drawImage( space, 0, 0 );
+        final ArrayList<String> input = new ArrayList<String>();
+ 
+        theScene.setOnKeyPressed(
+            new EventHandler<KeyEvent>() {
+                public void handle(KeyEvent e) {
+                    String code = e.getCode().toString();
+ 
+                    // only add once... prevent duplicates
+                    if ( !input.contains(code) ) {
+                        input.add( code );
+                        gc.clearRect(0, 0, 800, 800);
+                        System.out.println("Painettu " + code);
+                    }
+                    
+                }
+            });
+        
+         theScene.setOnKeyReleased(
+            new EventHandler<KeyEvent>()
+            {
+                public void handle(KeyEvent e)
+                {
+                    String code = e.getCode().toString();
+                    input.remove( code );
+                }
+            });
+        
+        //Tehdään menupainike
+        gc.setFill(Color.GREY);
+        gc.setStroke(Color.BLACK);
+        gc.setLineWidth(5);
+        gc.fillRect(0, 0, 300, 50);
+        gc.strokeRect(0, 0, 300, 50);
+        gc.setFill(Color.RED);
+        theFont = Font.font( "Times New Roman", FontWeight.BOLD, 20 );
+        gc.setFont( theFont );
+        gc.fillText("PLAY", 10, 20);
+         
+        VBox valikko = new VBox();
+        valikko.setSpacing(10);
+//        valikko.getChildren().add(jotain)
+        
+        
         stage.show();
         
     }
--- harjoitustyo/documentation/tuntikirjanpito.md
+++ harjoitustyo/documentation/tuntikirjanpito.md
@@ -5,3 +5,4 @@
  27.03.18 | 1h | Sovelluksen vaatimusmäärittely ja työaikakirjanpidon pystyttäminen.
  27.03.18 | 1,5h | Mavenin käyttöönotto. Kaikenmaailman säätöä joutui taas tekemään.
  27.03.18 | 1h | Tutustuttu javaFX:ään. Ensimmäinen ikkuna luotu.
+ 28.03.18 | 1,5h | Opeteltu javaFX:n käyttöä. Lisätty näppäimistönkuuntelija alkunäyttöön.
