73197aad0bafd9c37f0a1ea54c0d6eab3e8dd59e
Pate1337 <paavo.hemmo@hotmail.com>
2018-04-08 15:21:09 +0300

    Menuun lisätty varmistusikkuna

--- SpaceInvaders/src/main/java/com/mycompany/spaceinvaders/MainApp.java
+++ SpaceInvaders/src/main/java/com/mycompany/spaceinvaders/MainApp.java
@@ -29,6 +29,7 @@ import javafx.scene.control.Button;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
@@ -39,12 +40,14 @@ import javafx.stage.WindowEvent;
 public class MainApp extends Application {
     private String backGroundImage;
     private Scene startScene;
+    private Scene menuScene;
     private ImageView backGround;
     private Stage mainStage;
     private Group mainGroup;
     private String selected;
     private String prevSelected = "none";
     private boolean gamePaused = false;
+    private Group menuGroup;
     
     @Override
     public void init() throws Exception {
@@ -68,9 +71,9 @@ public class MainApp extends Application {
         System.out.println("Moro");
         selected = "none";
 //        mainGroup.getChildren().clear();
-        Group menuGroup = new Group();
+        menuGroup = new Group();
         menuGroup.getChildren().add(backGround);
-        Scene menuScene = new Scene(menuGroup);
+        menuScene = new Scene(menuGroup);
         mainStage.setScene(menuScene);
         
         
@@ -84,15 +87,15 @@ public class MainApp extends Application {
         final VBox menu = new VBox();
         menu.setSpacing(10);
         if (gamePaused) {
-            menu.getChildren().add(createMenuButton("Resume"));
+            menu.getChildren().add(createMenuButton("Resume", 400, 50, 40));
         } else {
-            menu.getChildren().add(createMenuButton("Play"));
+            menu.getChildren().add(createMenuButton("Play", 400, 50, 40));
         }
-        menu.getChildren().add(createMenuButton("Highscores"));
+        menu.getChildren().add(createMenuButton("Highscores", 400, 50, 40));
         if (gamePaused) {
-            menu.getChildren().add(createMenuButton("Exit to main menu"));
+            menu.getChildren().add(createMenuButton("Exit to main menu", 400, 50, 40));
         } else {
-            menu.getChildren().add(createMenuButton("Quit"));
+            menu.getChildren().add(createMenuButton("Quit", 400, 50, 40));
         }
         menu.setLayoutX(200);
         menu.setLayoutY(400);
@@ -250,38 +253,41 @@ public class MainApp extends Application {
                     drawGame();
                     this.stop();
                 } else if (selected.equals("QuitApp")) {
-                    System.out.println("Nyt pitäis apin sulkeutua");
-                    Platform.exit();
-                    System.exit(0);
+//                    System.out.println("Nyt pitäis apin sulkeutua");
+//                    Platform.exit();
+//                    System.exit(0);
+                    drawConfirm("Quit");
+                    this.stop();
                 } else if (selected.equals("ExitToMenu")) {
-                    System.out.println("Exit to menu");
-                    gamePaused = false;
-                    drawMenu();
+//                    System.out.println("Exit to menu");
+//                    gamePaused = false;
+//                    drawMenu();
+                    drawConfirm("Exit to main menu");
                     this.stop();
                 } else if (!gamePaused && !selected.equals("none") && !prevSelected.equals(selected)) { // && !inputs.isEmpty()
                     System.out.println("Piirretään koska !== none");
                     menu.getChildren().clear();
-                    menu.getChildren().add(createMenuButton("Play"));
-                    menu.getChildren().add(createMenuButton("Highscores"));
-                    menu.getChildren().add(createMenuButton("Quit"));
+                    menu.getChildren().add(createMenuButton("Play", 400, 50, 40));
+                    menu.getChildren().add(createMenuButton("Highscores", 400, 50, 40));
+                    menu.getChildren().add(createMenuButton("Quit", 400, 50, 40));
                 } else if (!gamePaused && selected.equals("none") && !prevSelected.equals(selected)) {
                     System.out.println("Piirretään koska none");
                     menu.getChildren().clear();
-                    menu.getChildren().add(createMenuButton("Play"));
-                    menu.getChildren().add(createMenuButton("Highscores"));
-                    menu.getChildren().add(createMenuButton("Quit"));
+                    menu.getChildren().add(createMenuButton("Play", 400, 50, 40));
+                    menu.getChildren().add(createMenuButton("Highscores", 400, 50, 40));
+                    menu.getChildren().add(createMenuButton("Quit", 400, 50, 40));
                 } else if (gamePaused && !selected.equals("none") && !prevSelected.equals(selected)) {
                     System.out.println("Piirretään koska !== none");
                     menu.getChildren().clear();
-                    menu.getChildren().add(createMenuButton("Resume"));
-                    menu.getChildren().add(createMenuButton("Highscores"));
-                    menu.getChildren().add(createMenuButton("Exit to main menu"));
+                    menu.getChildren().add(createMenuButton("Resume", 400, 50, 40));
+                    menu.getChildren().add(createMenuButton("Highscores", 400, 50, 40));
+                    menu.getChildren().add(createMenuButton("Exit to main menu", 400, 50, 40));
                 } else if (gamePaused && selected.equals("none") && !prevSelected.equals(selected)) {
                     System.out.println("Piirretään koska none");
                     menu.getChildren().clear();
-                    menu.getChildren().add(createMenuButton("Resume"));
-                    menu.getChildren().add(createMenuButton("Highscores"));
-                    menu.getChildren().add(createMenuButton("Exit to main menu"));
+                    menu.getChildren().add(createMenuButton("Resume", 400, 50, 40));
+                    menu.getChildren().add(createMenuButton("Highscores", 400, 50, 40));
+                    menu.getChildren().add(createMenuButton("Exit to main menu", 400, 50, 40));
                 }
                 //Piirretään vain kun tapahtuu muutos, ei turhaan
                 prevSelected = selected;
@@ -291,24 +297,24 @@ public class MainApp extends Application {
         mainStage.show();
     }
     
-    public Node createMenuButton(final String text) {
-        Canvas canvas = new Canvas(400, 50);
+    public Node createMenuButton(final String text, double width, double height, double fontSize) {
+        Canvas canvas = new Canvas(width, height);
         GraphicsContext gc = canvas.getGraphicsContext2D();
         if (selected.equals(text)) {
             gc.setFill(Color.YELLOW);
-            gc.fillRect(0, 0, 400, 50);
+            gc.fillRect(0, 0, width, height);
         } else {
             gc.setFill(Color.BLACK);
-            gc.fillRect(0, 0, 400, 50);
+            gc.fillRect(0, 0, width, height);
         }
 
         gc.setFill(Color.GREY);
-        gc.fillRect(4, 4, 392, 42);
+        gc.fillRect(4, 4, width - 8, height - 8);
         
         gc.setFill(Color.RED);
         gc.setStroke(Color.BLACK);
         gc.setLineWidth(2);
-        Font theFont = Font.font( "Times New Roman", FontWeight.BOLD, 40 );
+        Font theFont = Font.font( "Times New Roman", FontWeight.BOLD, fontSize );
         gc.setFont( theFont );
         gc.setTextAlign(TextAlignment.CENTER);
         gc.setTextBaseline(VPos.CENTER);
@@ -322,6 +328,189 @@ public class MainApp extends Application {
             Math.round(canvas.getHeight() / 2)
         );
 
+        return canvas;
+    }
+    
+    public void drawConfirm(final String text) {
+        selected = "none";
+        Node areYouSure = areYouSure(text);
+        areYouSure.setLayoutX(200);
+        areYouSure.setLayoutY(300);
+        
+        Rectangle behind = new Rectangle(0, 0, 800, 800);
+        behind.setFill(Color.BLACK);
+        behind.setOpacity(0.9);
+        menuGroup.getChildren().add(behind);
+        menuGroup.getChildren().add(areYouSure);
+        
+        final HBox buttons = new HBox();
+        buttons.setSpacing(20);
+        buttons.getChildren().add(createMenuButton(text, 160, 40, 15));
+        buttons.getChildren().add(createMenuButton("Cancel", 160, 40, 15));
+        buttons.setLayoutX(230);
+        buttons.setLayoutY(400);
+        menuGroup.getChildren().add(buttons);
+        
+        Rectangle menuTarget1 = new Rectangle(230, 400, 160, 40);
+        menuTarget1.setOpacity(0.0);
+        Rectangle menuTarget2 = new Rectangle(410, 400, 160, 40);
+        menuTarget2.setOpacity(0.0);
+        menuGroup.getChildren().add(menuTarget1);
+        menuGroup.getChildren().add(menuTarget2);
+        
+        menuTarget1.setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent e) {
+                System.out.println("Ollaan tekstin päällä");
+                if (text.equals("Quit")) {
+                    selected = "Quit";
+                } else {
+                    selected = "Exit to main menu";
+                }
+            }
+        });
+        menuTarget1.setOnMouseExited(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent e) {
+                System.out.println("Ei olla enää tekstin päällä");
+                selected = "none";
+            }
+        });
+        menuTarget1.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent e) {
+                if (text.equals("Quit")) {
+                    selected = "QuitApp";
+                } else {
+                    selected = "ExitToMenu";
+                }
+            }
+        });
+        menuTarget2.setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent e) {
+                System.out.println("Ollaan Cancelin päällä");
+                selected = "Cancel";
+            }
+        });
+        menuTarget2.setOnMouseExited(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent e) {
+                System.out.println("Ei olla enää Cancelin päällä");
+                selected = "none";
+            }
+        });
+        menuTarget2.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent e) {
+                selected = "goBack";
+            }
+        });
+        menuScene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent e) {
+                String code = e.getCode().toString();
+                if (code.equals("A")) {
+                    System.out.println("A painettu");
+                    if (selected.equals("none")) {
+                        selected = text;
+                    } else if (selected.equals(text)) {
+                        selected = "Cancel";
+                    } else if (selected.equals("Cancel")) {
+                        selected = text;
+                    }
+                } else if (code.equals("D")) {
+                    System.out.println("D painettu");
+                    if (selected.equals("none")) {
+                        selected = text;
+                    } else if (selected.equals(text)) {
+                        selected = "Cancel";
+                    } else if (selected.equals("Cancel")) {
+                        selected = text;
+                    }
+                } else if (code.equals("ENTER")) {
+                    if (selected.equals("Cancel")) {
+                        selected = "goBack";
+                    } else if (selected.equals("Quit")) {
+                        selected = "QuitApp";
+                    } else if (selected.equals("Exit to main menu")) {
+                        selected = "ExitToMenu";
+                    }
+                }
+            }
+        });
+        
+        
+        new AnimationTimer() {
+            @Override
+            public void handle(long currentNanoTime) {
+                if (selected.equals("Quit") && !prevSelected.equals(selected)) {
+                    buttons.getChildren().clear();
+                    buttons.getChildren().add(createMenuButton("Quit", 160, 40, 15));
+                    buttons.getChildren().add(createMenuButton("Cancel", 160, 40, 15));
+                } else if (selected.equals("Exit to main menu") && !prevSelected.equals(selected)) {
+                    buttons.getChildren().clear();
+                    buttons.getChildren().add(createMenuButton("Exit to main menu", 160, 40, 15));
+                    buttons.getChildren().add(createMenuButton("Cancel", 160, 40, 15));
+                } else if (selected.equals("Cancel") && !prevSelected.equals(selected)) {
+                    buttons.getChildren().clear();
+                    buttons.getChildren().add(createMenuButton(text, 160, 40, 15));
+                    buttons.getChildren().add(createMenuButton("Cancel", 160, 40, 15));
+                } else if (selected.equals("none") && !prevSelected.equals(selected)) {
+                    buttons.getChildren().clear();
+                    buttons.getChildren().add(createMenuButton(text, 160, 40, 15));
+                    buttons.getChildren().add(createMenuButton("Cancel", 160, 40, 15));
+                } else if (selected.equals("QuitApp")) {
+                    System.out.println("Bye bye");
+                    Platform.exit();
+                    System.exit(0);
+                } else if (selected.equals("ExitToMenu")) {
+                    gamePaused = false;
+                    drawMenu();
+                    this.stop();
+                } else if (selected.equals("goBack")) {
+                    drawMenu();
+                    this.stop();
+                }
+                prevSelected = selected;
+            }
+        }.start();
+    }
+    
+    public Node areYouSure(String text) {
+        Canvas canvas = new Canvas(400, 150);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        
+        gc.setFill(Color.BLACK);
+        gc.fillRect(0, 0, 400, 150);
+        gc.setFill(Color.GREY);
+        gc.fillRect(6, 6, 388, 138);
+        
+        gc.setFill(Color.RED);
+        gc.setStroke(Color.BLACK);
+        gc.setLineWidth(2);
+        Font theFont = Font.font( "Times New Roman", FontWeight.BOLD, 20 );
+        gc.setFont( theFont );
+        gc.setTextAlign(TextAlignment.CENTER);
+        gc.setTextBaseline(VPos.CENTER);
+        gc.fillText(
+            "Are you sure you want to", 
+            Math.round(canvas.getWidth()  / 2), 
+            Math.round(canvas.getHeight() / 4)
+        );
+        gc.fillText(text + "?",
+            Math.round(canvas.getWidth() / 2),
+            Math.round(canvas.getHeight() / 2.5)
+        );
+        gc.strokeText("Are you sure you want to", 
+            Math.round(canvas.getWidth()  / 2), 
+            Math.round(canvas.getHeight() / 4)
+        );
+        gc.strokeText(text + "?", 
+            Math.round(canvas.getWidth()  / 2), 
+            Math.round(canvas.getHeight() / 2.5)
+        );
+        
         return canvas;
     }
     
@@ -367,6 +556,8 @@ public class MainApp extends Application {
             {
                 //Tänne mitä piirretään
                 gc.clearRect(0, 0, 800, 800);
+                gc.setFill(Color.BLUE);
+                gc.fillRect(100, 100, 50, 50);
                 if (input.contains("A")) {
                     gc.setFill(Color.RED);
                     gc.fillRect(200, 400, 30, 30);
--- harjoitustyo/documentation/tuntikirjanpito.md
+++ harjoitustyo/documentation/tuntikirjanpito.md
@@ -12,3 +12,4 @@
  06.04.18 | 1,5h | Toiminnallisuus nyt sillä mallilla, että itse peliä voi ruveta koodaamaan. Playtä painaessa käynnistetään AnimationTimer.
  06.04.18 | 2h | Menun toimintaa paranneltu. Pystyy hallitsemaan näppäimistöllä ja hiirellä. Painikkeet muuttavat väriään kun ne ovat valittuina.
  06.04.18 | 2h | Menuun lisätty painikkeita. Painikkeet vaihtuvat riippuen laitetaanko peli paussille escapella vai ollaanko Main menussa.
+ 08.04.18 | 3,5h | Menuun lisätty varmistusikkuna, kun painetaan Quit tai Exit to main Menu.
